aaf1d72b38c975b61e80519da0c61737253e38fb
Leonardo Omeara <leonardogarzon32@outlook.com>
2023-08-12 20:22:52 -0500

    last nav fixes includin the already logged in nav

--- components/Nav.js
+++ components/Nav.js
@@ -6,16 +6,36 @@ import { useAuthState } from "react-firebase-hooks/auth";
 export default function Nav(){
 
     const [user, loading] = useAuthState(auth);
+    console.log(user);
 
     return(
-        <nav className="flex justify-between items-center py-10">
+        
+        <nav className="flex justify-between items-center py-10 px-3">
+            
             <Link href='/'>
                 <button className="text-lg font-medium">Post-It</button>
             </Link>
             <ul className="flex items-center gap-10">
+            {!user && (
                 <Link href={'/auth/login'}>
-                    <div className="py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8">Join Now</div>
+                    <div className="py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8">
+                    Join Now
+                    </div>
                 </Link>
+                )}
+            {user && (
+                <div className="flex items-center gap-8 text-sm">
+                    <Link href={'/post'}>
+                        <div className= "bg-slate-950 text-white p-4">Start posting</div>
+                    </Link>
+                    <Link href={'/dashboard'}>
+                        <img
+                         src={user.photoURL}
+                         className="w-10 rounded-full cursor-pointer"/>
+                        {/* <div className= "bg-slate-950 text-white p-4">Dashboard</div> */}
+                    </Link>
+                </div>
+            )}
             </ul>
         </nav>
     );
