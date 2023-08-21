a91e68effcee3f1d6ac790ffe0a310d5d0c1830a
Leonardo Omeara <leonardogarzon32@outlook.com>
2023-08-21 00:05:26 -0500

    Build the base of the post page, prevent the page to autorefresh when posting aswell

--- components/Layout.js
+++ components/Layout.js
@@ -2,7 +2,7 @@ import Nav from "./Nav";
 
 export default function Layout({ children }){
     return(
-        <div className="mx-6 md:max-w-2xl md:mx-auto bg-red-300">
+        <div className="mx-6 md:max-w-2xl md:mx-auto">
             <Nav />
             {/* <h1 className="text-3xl"> hola hola</h1> */}
             <main>{children}</main>
