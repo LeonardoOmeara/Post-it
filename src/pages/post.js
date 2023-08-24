c2d1e869e1eaa9f797a43c0033b5edf34d36215d
Leonardo Omeara <leonardogarzon32@outlook.com>
2023-08-24 00:08:00 -0500

    Set up the Post function with firestore, install Toastify and evaluate if the post its empty or too long to display the Toastif notification

--- src/pages/post.js
+++ src/pages/post.js
@@ -1,17 +1,59 @@
-import { auth } from "../../utils/firebase";
+import { auth, db } from "../../utils/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { useRouter } from "next/router";
+import { Router, useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { toast } from "react-toastify";
 
 
 export default function Post(){
 
     // Form state
     const [post, setPost] = useState({ description: ""});
+    const [user, loading] = useAuthState(auth);
+    const router = useRouter();
+   
+    // console.log("hola");
+    // console.log(user);
+    // console.log("hola");
 
     // Submit post
     const submitPost = async (e) => {
-        e.preventDefault();
+        e.preventDefault();// Prevents the form from being submitted and page refresh
+
+        // first, validate if post its correct to submit with this checks
+
+        if(!post.description){
+            toast.error("Description it's empty :(", {
+                position: toast.POSITION.TOP_CENTER,
+                autoClose: 1300});
+            // if i dont put the return here, the page will refresh
+            return;
+        }
+        //create the condition where if the post lenght is more than 300 characters, use toast.error and limit the max lenght in the textarea  to 300;
+        if (post.description.length > 300){
+            toast.error("Description it's too long :$", {
+                position: toast.POSITION.TOP_CENTER,
+                autoClose: 1300});
+                
+                // setPost({ description: ""});
+            return;
+
+        }
+        
+
+        const collectionRef = collection(db, "posts");
+        await addDoc(collectionRef, {
+            // Spread operator include all the properties of the post object
+            ...post,
+            timestamp: serverTimestamp(),
+            user: user.uid,
+            photo: user.photoURL,
+            username: user.displayName
+        })
+        // Clean the post textarea and redirect to the main page
+        setPost({ description: ""});
+        return router.push("/");
     }
 
     return (
